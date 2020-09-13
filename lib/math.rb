42077465174b41881e9e61ff26b53d772aa2bf25
Jacob Kagon <jacob@kagon.net>
2020-09-13 00:34:17 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -11,12 +11,14 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
+  
 
 end
 
 def multiplication(num1, num2)
-  
-
+  product = num1 * num2
+  puts product
+  product
 end
 
 def modulo(num1, num2)
