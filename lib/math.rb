12c1665927ba825ab44f9d9133db66c138080c92
Jacob Kagon <jacob@kagon.net>
2020-09-13 00:35:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -11,7 +11,9 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  
+  quotient = num2 / num1
+  puts quotient
+  quotient
 
 end
 
@@ -22,9 +24,13 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
+  remainder = num2 % num1
+  puts remainder
+  remainder
 
 end
 
 def square_root(num)
+  
 
 end
